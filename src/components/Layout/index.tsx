8545d94a46599963ef51aa1500c61d95f95c0268
luzhaopan <745003249@qq.com>
2023-12-06 12:19:16 +0800

    feat: layout

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,59 @@
+'use client'
+import { Inter } from 'next/font/google'
+import React, { useState } from 'react';
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined
+} from '@ant-design/icons';
+import { Layout, Button, theme } from 'antd';
+
+import MenuBar from './components/Menu';
+
+const { Header, Sider, Content } = Layout;
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function LayoutPage({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
+  return (
+    <Layout>
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <div className="demo-logo-vertical" />
+        <MenuBar />
+      </Sider>
+      <Layout>
+        <Header style={{ padding: 0, background: colorBgContainer }}>
+          <Button
+            type="text"
+            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            onClick={() => setCollapsed(!collapsed)}
+            style={{
+              fontSize: '16px',
+              width: 64,
+              height: 64,
+            }}
+          />
+        </Header>
+        <Content
+          style={{
+            margin: '24px 16px',
+            padding: 24,
+            minHeight: 280,
+            background: colorBgContainer,
+          }}
+        >
+          <div className={inter.className}>{children}</div>
+        </Content>
+      </Layout>
+    </Layout>
+  )
+}
